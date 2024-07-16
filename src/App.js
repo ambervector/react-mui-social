2049336816e365470ce6620d8fb5be95af590a39
Tuggu <tuggu@Tuggus-MacBook-Pro.local>
2024-07-16 17:40:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "@mui/material";
+import { styled } from "@mui/material";
+import { SideBar } from "./components/SideBar";
+import { Navbar } from "./components/Navbar";
+import { Feed } from "./components/Feed";
+import { RightBar } from "./components/RightBar";
+import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Navbar />
+      <Stack direction="row" justifyContent={"space-between"} gap={2}>
+        <SideBar />
+        <Feed />
+        <RightBar />
+      </Stack>
+    </Box>
   );
 }
 
